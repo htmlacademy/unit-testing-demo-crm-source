d7982e601ad544073702cc4c7f25464a248b5944
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-04-21 11:57:26 +0300

    adds design assets

--- crm-demo/src/index.tsx
+++ crm-demo/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import './sass/style.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
