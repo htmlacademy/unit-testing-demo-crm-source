b31618ed972f5e47d5c0ad69fc216c67cb715fd5
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-04-26 12:07:07 +0300

    adds buttons on the toolbar

--- crm-demo/src/components/filter-form/filter-form.tsx
+++ crm-demo/src/components/filter-form/filter-form.tsx
@@ -1,5 +1,15 @@
 import classnames from 'classnames';
+import { Button } from '../button/button';
 import { DownCrossSelect } from '../select/select-down';
+import {ReactComponent as IconLookThrough} from '../../img/sprite/icon-look-through.svg';
+import {ReactComponent as IconRollback} from '../../img/sprite/icon-roll-back.svg';
+import {ReactComponent as IconReset} from '../../img/sprite/icon-reset.svg';
+import {ReactComponent as IconArrowInCircle } from '../../img/sprite/icon-arrow-in-circle.svg';
+const BUTTON_ICON_SIZE = 24;
+const BUTTON_SMALL_SIZE = 20;
+const BUTTON_CLASS_NAME = 'button--lighter button--large-icon filter-form__button';
+
+
 interface FilterFormProps{
   className:string;
 }
@@ -14,7 +24,46 @@ export function FilterForm({className}:FilterFormProps) {
 
         </div>
         <div className='filter-form__buttons-wrapper'>
-
+          <Button
+            leftIcon={
+              <IconLookThrough
+                width={BUTTON_ICON_SIZE}
+                height={BUTTON_ICON_SIZE}
+              />
+            }
+            type="button"
+            className={BUTTON_CLASS_NAME}
+          />
+          <Button
+            leftIcon={
+              <IconRollback
+                width={BUTTON_ICON_SIZE}
+                height={BUTTON_ICON_SIZE}
+              />
+            }
+          />
+          <Button
+            caption='Вернуть'
+            leftIcon={
+              <IconReset
+                width={BUTTON_SMALL_SIZE}
+                height={BUTTON_SMALL_SIZE}
+              />
+            }
+            type="button"
+            className='button--transparent filter-form__button'
+          />
+          <Button
+            caption='Изменить состояние'
+            rightIcon={
+              <IconArrowInCircle
+                width={BUTTON_SMALL_SIZE}
+                height={BUTTON_SMALL_SIZE}
+              />
+            }
+            type="button"
+            className='button--filled filter-form__button'
+          />
         </div>
       </form>
     </div>
