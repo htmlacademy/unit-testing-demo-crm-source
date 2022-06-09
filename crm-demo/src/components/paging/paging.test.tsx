55a4d6ade59f01face3667ee493200c179cb364e
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-06-09 11:25:53 +0300

    completes the demo. before home-work

--- crm-demo/src/components/paging/paging.test.tsx
+++ crm-demo/src/components/paging/paging.test.tsx
@@ -1,30 +1,58 @@
 import { render, screen } from '@testing-library/react';
 import { ComponentType } from 'react';
-import { CurrentPageSelector, Paging, SelectCurrentPageProps } from './paging';
+import { CurrentPageSelector, DataTable, DataTableProps, Paging } from './paging';
 
 describe('Компонент управления разбиением на страницы', () => {
   it('требует для работы размер страницы', () => {
     const pageSize = 1;
     const emptyMessage: ComponentType = () => null;
-    render(<Paging pageSize={pageSize} emptyMessage={emptyMessage} />);
+    const selectCurrentPage = jest.fn() as CurrentPageSelector;
+    const table = jest.fn() as DataTable<unknown>;
+    render(
+      <Paging
+        pageSize={pageSize}
+        emptyMessage={emptyMessage}
+        selectCurrentPage={selectCurrentPage}
+        table={table}
+      />
+    );
   });
   it.each([-1, 0, 1.5])('прекращает работу с ошибкой когда получает размер страницы (%d) не являющийся целым положительным числом', (pageSize) => {
     const emptyMessage: ComponentType = () => null;
-    expect(() => render(<Paging pageSize={pageSize} emptyMessage={emptyMessage} />)).toThrow();
+    const selectCurrentPage = jest.fn() as CurrentPageSelector;
+    const table = jest.fn() as DataTable<unknown>;
+    expect(() => render(
+      <Paging
+        pageSize={pageSize}
+        emptyMessage={emptyMessage}
+        selectCurrentPage={selectCurrentPage}
+        table={table}
+      />
+    )).toThrow();
   });
   it('требует сведений для изображения сообщения в случае отсутствия данных', () => {
     const pageSize = 1;
     const message = 'empty list';
     const emptyMessage: ComponentType = () => (<div>{message}</div>);
-    render(<Paging pageSize={pageSize} emptyMessage={emptyMessage} />);
+    const selectCurrentPage = jest.fn() as CurrentPageSelector;
+    const table = jest.fn() as DataTable<unknown>;
+    render(
+      <Paging
+        pageSize={pageSize}
+        emptyMessage={emptyMessage}
+        selectCurrentPage={selectCurrentPage}
+        table={table}
+      />
+    );
     const result = screen.getByText(message);
     expect(result).not.toBeNull();
   });
   it('использует бизнес правило для определения текущей страницы', () => {
     const pageSize = 1;
     const emptyMessage = () => null;
-    const rule = jest.fn() as CurrentPageSelector;
+    const rule = jest.fn(() => ({ start: 0, end: 0 })) as CurrentPageSelector;
     const items = Array.from({ length: 1 });
+    const table = jest.fn() as DataTable<unknown>;
 
     render(
       <Paging
@@ -32,8 +60,28 @@ describe('Компонент управления разбиением на ст
         emptyMessage={emptyMessage}
         selectCurrentPage={rule}
         items={items}
+        table={table}
       />
     );
     expect(rule).toBeCalled();
+  });
+  it('делегирует рендеринг страницы специальному компоненту', () => {
+    const pageSize = 1;
+    const emptyMessage = () => null;
+    const rule = jest.fn(() => ({ start: 1, end: 3 })) as CurrentPageSelector;
+    const items = [10, 11, 12, 13];
+    const table = ({ page }: DataTableProps<number>) => <div>{page.toString()}</div>;
+
+    render(
+      <Paging
+        pageSize={pageSize}
+        emptyMessage={emptyMessage}
+        selectCurrentPage={rule}
+        items={items}
+        table={table}
+      />
+    );
+    const result = screen.getByText('11,12');
+    expect(result).toBeInTheDocument();
   });
 });
--- crm-demo/src/components/paging/paging.tsx
+++ crm-demo/src/components/paging/paging.tsx
@@ -6,24 +6,53 @@ export interface SelectCurrentPageProps {
   requestedPage?: number | undefined;
 }
 
-export type CurrentPageSelector = (props: SelectCurrentPageProps) => number;
+export interface PageBoundaries {
+  start: number;
+  end: number;
+}
+
+export interface DataTableProps<T> {
+  page: T[]
+}
+
+export type DataTable<T> = ComponentType<DataTableProps<T>>;
+
+export type CurrentPageSelector = (props: SelectCurrentPageProps) => PageBoundaries;
 
 export interface PagingProps<T> {
   pageSize: number;
   emptyMessage: ComponentType;
   items?: T[] | undefined;
   selectCurrentPage: CurrentPageSelector;
+  table: DataTable<T>;
+  requestedPage?: number | undefined;
 }
 
-const isEmpty = <T,>(items: T[] | undefined) => (!Array.isArray(items) || items.length <= 0);
+const hasData = <T,>(items: T[] | undefined): items is T[] => (Array.isArray(items) && items.length > 0);
 
 export function Paging<T>(props: PagingProps<T>) {
-  const { pageSize, emptyMessage: Empty, items } = props;
+  const {
+    pageSize,
+    emptyMessage: Empty,
+    items,
+    selectCurrentPage,
+    table: Table,
+    requestedPage,
+  } = props;
   if (!Number.isInteger(pageSize) || pageSize <= 0) {
     throw new Error();
   }
-  if (isEmpty(items)) {
+  if (!hasData(items)) {
     return <Empty />;
   }
-  return null;
+  const {start, end} = selectCurrentPage({
+    pageSize,
+    totalItems: items.length,
+    requestedPage,
+  });
+  return (
+    <Table
+      page={items.slice(start, end)}
+    />
+  );
 }
