1e85b36c9851417c5db67dee0073bcca48584449
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-06-09 08:08:36 +0300

    adds empty list behaveior

--- crm-demo/src/components/paging/paging.tsx
+++ crm-demo/src/components/paging/paging.tsx
@@ -1,14 +1,20 @@
 import { ComponentType } from 'react';
 
-export interface PagingProps{
-  pageSize:number;
+export interface PagingProps<T> {
+  pageSize: number;
   emptyMessage: ComponentType;
+  items?: T[] | undefined
 }
 
-export function Paging(props:PagingProps){
-  const {pageSize} = props;
-  if(!Number.isInteger(pageSize) || pageSize<=0){
+const isEmpty = <T,>(items: T[] | undefined) => (!Array.isArray(items) || items.length <= 0);
+
+export function Paging<T>(props: PagingProps<T>) {
+  const { pageSize, emptyMessage: Empty, items } = props;
+  if (!Number.isInteger(pageSize) || pageSize <= 0) {
     throw new Error();
+  }
+  if (isEmpty(items)) {
+    return <Empty />;
   }
   return null;
 }
